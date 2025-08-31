3392beed92e108c71448dd6bb0d6da968bfba671
MdJakiulRashidKhan <jakiul15-8554@diu.edu.bd>
2025-08-31 14:24:15 +0600

    added

---
+++ assignment-1.py
@@ -0,0 +1,35 @@
+# 1. Print your name
+print("Md Jakiul Rashid Khan")
+
+# 2. Input your batch number using input() function
+batch_number = input("Enter your batch number: ")
+print("Batch Number:", batch_number)
+
+# 3. Create 5 different valid Variables and print
+name = "Jakiul"
+age = 23
+is_student = True
+cgpa = 3.75
+department = "CSE"
+
+print(name, age, is_student, cgpa, department)
+
+# 4. Create Pascal Case Variable
+MyBatchNumber = 25
+print("Pascal Case Variable:", MyBatchNumber)
+
+# 5. Assign a single value to Multiple variables
+x = y = z = 100
+print(x, y, z)
+
+# 6. Input float value using input()
+float_value = float(input("Enter a float value: "))
+print("Float Value:", float_value)
+
+# 7. Convert into upper case
+a = "Welcome to Django for web and Ai"
+print(a.upper())
+
+# 8. Remove whitespace
+b = " Python"
+print(b.strip())
